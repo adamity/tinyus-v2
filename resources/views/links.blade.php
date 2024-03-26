efc6b48e75b2c46124b061d570503e947c195412
Adamity <50904284+adamity@users.noreply.github.com>
2024-03-27 00:18:43 +0800

    Refactor links.blade.php layout and add view button to each card

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -4,29 +4,29 @@
 <div class="bg-milk-punch-highland border-bottom border-3 border-dark">
     <div class="bg-noise">
         <div class="container">
-            <p class="fw-bold fs-1 ff-fugaz text-center pt-5 mb-5">Here's your pack of URLs! 📦</p>
+            <p class="fw-bold fs-1 ff-fugaz text-center pt-5">Here's your pack of URLs! 📦</p>
 
-            <div class="row">
+            <div class="row py-5">
                 <div class="col-lg-8 offset-lg-2">
                     @foreach ($original_urls as $original_url)
-                        <div class="card mb-3">
+                        <div class="card border border-3 border-dark shadow-none mb-3">
                             <div class="card-body">
-                                {{-- Title --}}
                                 <h5 class="card-title">{{ $original_url->preview ? $original_url->preview['title'] : 'N/A' }}</h5>
-                                {{-- Description --}}
                                 <p class="card-text">{{ $original_url->preview ? $original_url->preview['description'] : 'N/A' }}</p>
+
                                 <div class="d-flex align-items-center">
                                     <div class="me-3">
-                                        {{-- Favicon --}}
                                         <img src="{{ $original_url->preview ? $original_url->preview['favicon'] : asset('images/placeholder.png') }}" alt="" class="img-fluid">
                                     </div>
-                                    {{-- URL --}}
                                     <small>{{ $original_url->url }}</small>
                                 </div>
                             </div>
+                            <div class="card-footer p-0">
+                                <a href="{{ $original_url->preview ? $original_url->preview['url'] : '' }}" target="_blank" class="btn btn-warning border-top border-3 border-dark text-dark rounded-0 rounded-bottom shadow-none w-100">
+                                    View
+                                </a>
+                            </div>
                         </div>
-                        {{-- Thumbnail --}}
-                        {{-- <img src="{{ $original_url->preview ? $original_url->preview['thumbnail'] : asset('images/placeholder.png') }}" alt="" class="img-fluid"> --}}
                     @endforeach
                 </div>
             </div>
